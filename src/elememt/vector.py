02196163fc1955c9489cbc582308f11c38efe0c8
SunshineAllWay <mhnfwln@126.com>
2018-07-02 12:26:28 +0800

    define DIMENSION COMPONENT ADD in class vecotr

--- src/elememt/vector.py
+++ src/elememt/vector.py
@@ -12,7 +12,7 @@ Overview:
 - function randomVector(N,a,b)
 """
 
-import  math
+import math
 import random
 
 class Vector(object):
@@ -21,11 +21,13 @@ class Vector(object):
     def __init__(self, components):
         self.__components = components
 
+
     # set components in the vector
     def set(self, components):
         self.__components = components
 
-    # returns a string representation of the vector
+
+    # return a string representation of the vector
     def __str__(self):
         ans = "("
         length = len(self.__components)
@@ -37,5 +39,37 @@ class Vector(object):
         if len(ans) == 1:
             ans += ")"              # empty vector
         return ans
+
+
+    # return the dimension of vector
+    def dimension(self):
+        return len(self.__components)
+
+
+    # return the i-th component
+    def component(self, i):
+        if i < dimension(self):
+            return self.__components[i]
+        else:
+            return None
+
+    # add
+    def add(self, v):
+        if dimension(v) != dimension(self):
+            return None
+
+        length = dimension(self)
+        for i in range(length):
+            self.__components[i] += v.component(i)
+
+
+
+def main():
+    Vector v1([1,2,3])
+    Vector v2([2,3,4])
+    print (v1)
+
+if __name__ == "__main__":
+    main()
 
 
